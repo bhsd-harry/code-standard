b7e1822d6da5130971ac51968280674837ab32f1
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-09-30 13:04:47 +0800

    chore(eslintrc.dist.cjs): update to ES2019

--- eslintrc.dist.cjs
+++ eslintrc.dist.cjs
@@ -4,17 +4,19 @@ module.exports = {
 	root: true,
 	env: {
 		browser: true,
-		es2022: true,
+		es2024: true,
 	},
 	plugins: ['es-x'],
-	extends: ['plugin:es-x/restrict-to-es2018'],
+	extends: ['plugin:es-x/restrict-to-es2019'],
 	parserOptions: {
-		ecmaVersion: 13,
+		ecmaVersion: 'latest',
 	},
 	rules: {
-		'es-x/no-array-prototype-flat': 0,
+		'es-x/no-global-this': 0,
+		'es-x/no-import-meta': 0,
+		'es-x/no-promise-all-settled': 0,
 		'es-x/no-regexp-lookbehind-assertions': 2,
-		'es-x/no-string-prototype-trimstart-trimend': 0,
+		'es-x/no-string-prototype-matchall': 0,
 	},
 	settings: {
 		'es-x': {
