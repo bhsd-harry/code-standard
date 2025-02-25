50d2c68c4704f36e009216aa837c5d580bde533e
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-02-25 21:01:59 +0800

    fix(index): avoid repeated loading

--- src/index.ts
+++ src/index.ts
@@ -95,36 +95,45 @@ export const splitColors = (str: string, hsl = true): [string, number, number, b
 	return pieces;
 };
 
+const loading = new Map<string, Promise<void>>();
+
 /**
  * 使用传统方法加载脚本
  * @param src 脚本地址
  * @param globalConst 脚本全局变量名
  * @param amd 是否兼容 AMD
  */
-export const loadScript = (src: string, globalConst: string, amd?: boolean): Promise<void> => new Promise(resolve => {
-	const path = `${CDN}/${src}`;
-	let obj: Obj | undefined = globalThis as unknown as Obj;
-	for (const prop of globalConst.split('.')) {
-		obj = obj?.[prop];
+export const loadScript = (src: string, globalConst: string, amd?: boolean): Promise<void> => {
+	if (loading.has(src)) {
+		return loading.get(src)!;
 	}
-	if (obj) {
-		resolve();
-	} else if (amd && typeof define === 'function' && 'amd' in define) {
-		const requirejs = globalThis.require as unknown as Require;
-		requirejs.config({paths: {[globalConst]: path}});
-		requirejs([globalConst], (exports: unknown) => {
-			Object.assign(globalThis, {[globalConst]: exports});
+	const promise = new Promise<void>(resolve => {
+		const path = `${CDN}/${src}`;
+		let obj: Obj | undefined = globalThis as unknown as Obj;
+		for (const prop of globalConst.split('.')) {
+			obj = obj?.[prop];
+		}
+		if (obj) {
 			resolve();
-		});
-	} else {
-		const script = document.createElement('script');
-		script.src = path;
-		script.onload = (): void => {
-			resolve();
-		};
-		document.head.append(script);
-	}
-});
+		} else if (amd && typeof define === 'function' && 'amd' in define) {
+			const requirejs = globalThis.require as unknown as Require;
+			requirejs.config({paths: {[globalConst]: path}});
+			requirejs([globalConst], (exports: unknown) => {
+				Object.assign(globalThis, {[globalConst]: exports});
+				resolve();
+			});
+		} else {
+			const script = document.createElement('script');
+			script.src = path;
+			script.onload = (): void => {
+				resolve();
+			};
+			document.head.append(script);
+		}
+	});
+	loading.set(src, promise);
+	return promise;
+};
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getObject = (key: string): any => JSON.parse(String(localStorage.getItem(key)));
