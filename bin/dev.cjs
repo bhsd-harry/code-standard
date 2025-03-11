a430d969f0f558b335faa614240f374e074e57a8
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-03-11 22:42:13 +0800

    fix(bin/dev.cjs): ignore dependecies

--- bin/dev.cjs
+++ bin/dev.cjs
@@ -4,9 +4,11 @@
 const fs = require('fs'),
 	{devDependencies, version} = require('@bhsd/common/package.json'),
 	json = require(`${process.cwd()}/package.json`);
-const {dependencies} = json,
+const {name: n, dependencies, optionalDependencies} = json,
+	others = {[n]: '', ...dependencies, ...optionalDependencies},
 	dev = Object.fromEntries(
-		[...Object.entries({...json.devDependencies, ...devDependencies})].sort(([a], [b]) => a.localeCompare(b)),
+		Object.entries({...json.devDependencies, ...devDependencies}).filter(([k]) => !(k in others))
+			.sort(([a], [b]) => a.localeCompare(b)),
 	);
 (dependencies && '@bhsd/common' in dependencies ? dependencies : dev)['@bhsd/common'] = `^${version}`;
 json.devDependencies = dev;
